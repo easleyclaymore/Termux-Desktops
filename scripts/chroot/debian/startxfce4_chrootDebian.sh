f6be21d9f73e67538f9e434c8968e1afedc84639
easley <40049427+easleyclaymore@users.noreply.github.com>
2025-01-26 19:06:41 +0800

    Update startxfce4_chrootDebian.sh

--- scripts/chroot/debian/startxfce4_chrootDebian.sh
+++ scripts/chroot/debian/startxfce4_chrootDebian.sh
@@ -4,11 +4,11 @@
 killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-lock
 
 ## Start Termux X11
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
+#am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
 
 sudo busybox mount --bind $PREFIX/tmp /data/SubSystem/chrootDebian/tmp
 
-XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :0 -ac &
+#XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :0 -ac &
 
 sleep 3
 

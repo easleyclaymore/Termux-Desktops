4e4a11bc5b22d4c846c6744650dc71ff1dd7652d
easley <40049427+easleyclaymore@users.noreply.github.com>
2025-01-26 20:17:54 +0800

    Update start_debian.sh

--- scripts/chroot/debian/start_debian.sh
+++ scripts/chroot/debian/start_debian.sh
@@ -20,6 +20,7 @@ mkdir $DEBIANPATH/sdcard
 busybox mount --bind /sdcard $DEBIANPATH/sdcard
 
 # chroot into DEBIAN
-busybox chroot $DEBIANPATH /bin/su - root
+busybox chroot $DEBIANPATH /bin/su - root -c 'sudo apt install firefox-esr neofetch xfce4-goodies ibus ibus-libpinyin ufw locales libatomic1 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-render-util0 libxcb-keysyms1 tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer'
 #busybox chroot $DEBIANPATH /bin/su - root -c 'export XDG_RUNTIME_DIR=${TMPDIR} && export PULSE_SERVER=tcp:127.0.0.1:4713 && sudo service dbus start && su - droidmaster -c "env DISPLAY=:0 startxfce4"'
 #busybox chroot $DEBIANPATH /bin/su - alin -c 'export DISPLAY=:0 && export PULSE_SERVER=127.0.0.1 && dbus-launch --exit-with-session startxfce4'
+#busybox chroot $DEBIANPATH /bin/su - alin -c 'export DISPLAY=:1 && export PULSE_SERVER=127.0.0.1 && vncserver'

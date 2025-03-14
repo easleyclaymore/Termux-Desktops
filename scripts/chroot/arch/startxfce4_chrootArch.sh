c9c0c11c64ae1b380f2cfa936892f6f77bc0beb5
easley <40049427+easleyclaymore@users.noreply.github.com>
2025-03-14 09:09:39 +0800

    Update startxfce4_chrootArch.sh

--- scripts/chroot/arch/startxfce4_chrootArch.sh
+++ scripts/chroot/arch/startxfce4_chrootArch.sh
@@ -4,7 +4,7 @@ killall -9 termux-x11 Xwayland pulseaudio virgl_test_server_android termux-wake-
 
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
 
-sudo busybox mount --bind $PREFIX/tmp /data/local/tmp/chrootarch/tmp
+sudo busybox mount --bind $PREFIX/tmp /data/SubSystem/chrootarch/tmp
 
 XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :0 -ac &
 
@@ -17,4 +17,4 @@ virgl_test_server_android &
 
 sudo chmod -R 1777 /data/data/com.termux/files/usr/tmp # i put this as virgl kept throwing errors and this seemed to work
 
-su -c "sh /data/local/tmp/start_arch.sh"
+su -c "sh /data/SubSystem/start_arch.sh"
